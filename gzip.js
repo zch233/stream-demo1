878c8cbf0faa7041d26d03dc734ed90427090ba0
zhangchao <529743595@qq.com>
2020-06-07 16:36:17 +0800

    Update Gzip

--- gzip.js
+++ gzip.js
@@ -4,4 +4,6 @@ const file = process.argv[2]
 
 fs.createReadStream(file)
   .pipe(zlib.createGzip())
+  .on('data', () => console.log('.')) // 这里可以做进度条
   .pipe(fs.createWriteStream(file + '.gz'))
+  .on('finish', () => console.log('Done'))
