61e82a4b3d0c85d116ad05dd4f14d76b2d1fba81
zhangchao <529743595@qq.com>
2020-06-07 15:40:24 +0800

    Add Flag

--- file.js
+++ file.js
@@ -1,7 +1,9 @@
 const fs = require('fs')
 const stream = fs.createWriteStream('./file.txt')
 for (let i = 0; i < 1000000; i++) {
-  stream.write(`这是${i}行，这里将会有很多很多的内容，啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊\n`)
+  const flag = stream.write(`这是${i}行，这里将会有很多很多的内容，啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊\n`)
+  // flag：false 的意思是你写的太快了，数据积压了
+  console.log(flag, i)
 }
 stream.end()
 console.log('end')
