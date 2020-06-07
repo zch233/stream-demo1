36c00f0af81921633e5af9b847df9ca8a180ea9b
zhangchao <529743595@qq.com>
2020-06-07 14:42:49 +0800

    Add Stream

--- file.js
+++ file.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const stream = fs.createWriteStream('./file.txt')
 for (let i = 0; i < 1000000; i++) {
-  stream.write(`这是${i}行\n`)
+  stream.write(`这是${i}行，这里将会有很多很多的内容，啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊\n`)
 }
 stream.end()
 console.log('end')
--- server.js
+++ server.js
@@ -4,12 +4,18 @@ const http = require('http')
 const server = http.createServer()
 
 // 不用 stream 很占内存
+// server.on('request', (request, response) => {
+//   fs.readFile('./file.txt', (err, data) => {
+//     if (err) throw err
+//     response.end(data)
+//     console.log('done')
+//   })
+// })
+
+// 用 stream 的形式
 server.on('request', (request, response) => {
-  fs.readFile('./file.txt', (err, data) => {
-    if (err) throw err
-    response.end(data)
-    console.log('done')
-  })
+  const stream = fs.createReadStream('./file.txt')
+  stream.pipe(response)
 })
 
-server.listen(8888)
+server.listen(8888, () => console.log('listen: 8888'))
