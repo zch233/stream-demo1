5406a059d169992f9648e96fb50b49dac914bb3d
zhangchao <529743595@qq.com>
2020-06-07 14:28:16 +0800

    Fix Index File

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
-const stream = fs.createWriteStream('./file.text')
-for (let i = 0; i < 10000; i++) {
+const stream = fs.createWriteStream('./file.txt')
+for (let i = 0; i < 1000000; i++) {
   stream.write(`这是${i}行\n`)
 }
 stream.end()
