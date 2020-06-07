423e2d837013802fcd0e097bea665ca1ea18c695
zhangchao <529743595@qq.com>
2020-06-07 16:39:09 +0800

    Update Gzip

--- gzip.js
+++ gzip.js
@@ -2,8 +2,17 @@ const fs = require('fs')
 const zlib = require('zlib')
 const file = process.argv[2]
 
+const { Transform } = require('stream')
+const transform = new Transform({
+  transform (chunk, encoding, callback) {
+    console.log('.')
+    callback(null, chunk)
+  }
+})
+
+
 fs.createReadStream(file)
   .pipe(zlib.createGzip())
-  .on('data', () => console.log('.')) // 这里可以做进度条
+  .pipe(transform) // 这里可以做进度条，webpack 就是这样的原理，vue-loader => scss-loader => css-loader => style-loader
   .pipe(fs.createWriteStream(file + '.gz'))
   .on('finish', () => console.log('Done'))
